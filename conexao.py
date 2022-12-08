358a107aeaef46bd6fc5f10f4bca62a0b55f34da
LuizMiron <luiz.gugamiron@gmail.com>
2022-12-08 20:04:32 -0300

    segundo commit

--- conexao.py
+++ conexao.py
@@ -1,3 +1,15 @@
 import sqlite3
 
-banco = sqlite3.connect('banco.bd')
+def CriarBanco():
+    #Cria o banco.O cursor é responsavel pelos comandos sql
+    banco = sqlite3.connect('banco.bd')
+    cursor = banco.cursor()
+    
+    #cursor.execute("CREATE TABLE contas (usuario text, senha integer)")
+    
+    cursor.execute("INSERT INTO contas VALUES('adm',12345)")
+    banco.commit()
+
+
+
+CriarBanco()
